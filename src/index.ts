fc1862fa796079adfa9e2b731c203fe0f15cd0a1
Курилин Никита Андреевич <kurilinna@pochtabank.ru>
2024-09-24 23:47:13 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 5050;
+
+app.post('/', (req: Request, res: Response) => {
+	res.send('Hello from NodeJS');
+});
+
+const server = app.listen(PORT, () => {
+	console.log(`server started on port: ${PORT}`);
+});
+
+export { app, server };
+
+hello;
