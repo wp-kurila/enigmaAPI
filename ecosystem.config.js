201ce878c74f513b8aac49ec2c3f474ae004b3f2
Курилин Никита Андреевич <kurilinna@pochtabank.ru>
2024-09-29 15:38:50 +0700

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	apps: [
 		{
-			name: 'my-node-app',
+			name: 'enigmaApi',
 			script: 'npm',
 			args: 'start',
 			watch: false,
--- src/libs/telegram.ts
+++ src/libs/telegram.ts
@@ -10,9 +10,6 @@ const bot = new TelegramBot(token, { polling: true });
 bot.on('message', (msg: Message) => {
 	const chatId = msg.chat.id;
 
-	// Логируем chat_id в консоль
-	console.log(`Chat ID: ${chatId}`);
-
 	// Отправляем пользователю сообщение с его chat_id
 	bot.sendMessage(chatId, `Ваш chat_id: ${chatId}`);
 });
